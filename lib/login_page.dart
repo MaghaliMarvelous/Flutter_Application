635e1073939c60c3b5b1ab57158cd42e65df335f
MaghaliMarvelous <133017983+MaghaliMarvelous@users.noreply.github.com>
2025-08-05 09:14:41 +0700

    add another reusable components

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_pplg1_20/components/widget_button.dart';
 import 'package:flutter_application_pplg1_20/home_Page.dart';
 import 'dart:developer';
-
 import 'package:flutter_application_pplg1_20/register_page.dart';
 
 class LoginPage extends StatefulWidget {
@@ -15,112 +15,108 @@ class _LoginPageState extends State<LoginPage> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   String statuslogin = "Login status";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Login Page")),
       body: Container(
         margin: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: Text(
-                "Welcome to the Login Page!",
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.blueGrey,
-                  fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Text(
+                  "Welcome to the Login Page!",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.blueGrey,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-            ),
-            Center(child: Text("Please login using your email and password.")),
-            //image dari resources
-            SizedBox(height: 10),
-            Center(
-              child: Image.asset('assets/catpfptest.jpg', width: 100, height: 100),
-            ),
-            //buatlah isian username dan password
-            Container(
-              margin: EdgeInsets.only(top: 20, bottom: 10),
-              child: TextField(
-                controller: emailController,
+              Center(child: Text("Please login using your email and password.")),
+              SizedBox(height: 10),
+              Center(
+                child: Image.asset('assets/catpfptest.jpg', width: 100, height: 100),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 20, bottom: 10),
+                child: TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    hintText: "Enter your email",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              TextField(
+                controller: passwordController,
+                obscureText: true,
                 decoration: InputDecoration(
-                  labelText: "Email",
-                  hintText: "Enter your email",
+                  labelText: "Password",
+                  hintText: "Enter your password",
                   border: OutlineInputBorder(),
                 ),
               ),
-            ),
-            TextField(
-              controller: passwordController,
-              obscureText: true, // untuk menyembunyikan karakter password
-              decoration: InputDecoration(
-                labelText: "Password",
-                hintText: "Enter your password",
-                border: OutlineInputBorder(),
-              ),
-            ),
-            //button login
-            Center(
-              child: Container(
-                margin: EdgeInsets.only(top: 10),
-                width: 300,
-                child: ElevatedButton(
-                  onPressed: () {
-                    if (emailController.text == "admin" &&
-                        passwordController.text == "admin") {
-                      setState(() {
-                        statuslogin = "Login successful!";
-                        log(statuslogin);
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => HomePage()
-                          ),
-                        );
-                      });
-                    } else {
-                      setState(() {
-                        statuslogin = "Login Failed!";
-                        log(statuslogin);
-                      });
-                    }
-                  },
-                  child: Text("Login"),
-                  
-                ),
-              ),
-            ),
+              SizedBox(height: 20),
 
-            Center(
-              child: TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                    builder: (context) => RegisterPage()
+              Center(
+                child: Column(
+                  children: [
+                    Container(
+
+                      margin: EdgeInsets.only(top: 25),
+                      child: CustomButton(
+                        text: "Login",
+                        textColor: Colors.cyan,
+                        onPressed: () {
+                          if (emailController.text == "admin" &&
+                              passwordController.text == "admin") {
+                            setState(() {
+                              statuslogin = "Login successful!";
+                              log(statuslogin);
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => HomePage(),
+                                ),
+                              );
+                            });
+                          } else {
+                            setState(() {
+                              statuslogin = "Login Failed!";
+                              log(statuslogin);
+                            });
+                          }
+                        },
+                      ),
                     ),
-                  );
-                },
-                child: Text(
-                  "Don't have any account? Register!",
-                  style: TextStyle(
-                    color: const Color.fromARGB(255, 30, 30, 30),
-                    decoration: TextDecoration.underline,
-                  ),
+                    Container(
+                      margin: EdgeInsets.only(top: 25),
+                      child: CustomButton(
+                        text: "Register",
+                        textColor: Colors.green,
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => RegisterPage(),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-            Container(
-               margin: EdgeInsets.only(top: 20),
-              child: Center(
-                child: Text(
-                  statuslogin
-                  ),
-                ),
-            ),
-          ],
+
+              SizedBox(height: 20),
+              Center(child: Text(statuslogin)),
+            ],
+          ),
         ),
       ),
     );
--- lib/register_page.dart
+++ lib/register_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'components/custom_date_selector.dart';
-import '../components/custom_text_field.dart';
+import '../components/custom_date_selector.dart';
 import '../components/custom_gender_selector.dart';
-import 'components/logo_header.dart';
+import '../components/logo_header.dart';
+import '../components/widget_button.dart';
+import '../components/widget_textfield.dart'; 
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -40,30 +41,51 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Register')),
+      backgroundColor: Colors.grey.shade100,
+      appBar: AppBar(
+        title: const Text('Register'),
+        backgroundColor: Colors.teal,
+      ),
       body: SafeArea(
         child: SingleChildScrollView(
-          padding: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(20.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const LogoHeader(),
-              const SizedBox(height: 20),
-              CustomTextField(controller: emailController, label: 'Email or Username'),
-              const SizedBox(height: 10),
-              CustomTextField(controller: passwordController, label: 'Password', obscure: true),
-              const SizedBox(height: 20),
-              GenderSelector(selectedGender: _gender, onChanged: _onGenderSelected),
-              const SizedBox(height: 20),
+              const SizedBox(height: 30),
+
+              CustomTextField(
+                controller: emailController,
+                label: 'Email or Username',
+              ),
+              const SizedBox(height: 16),
+
+              CustomTextField(
+                controller: passwordController,
+                label: 'Password',
+                obscure: true,
+              ),
+              const SizedBox(height: 24),
+
+              GenderSelector(
+                selectedGender: _gender,
+                onChanged: _onGenderSelected,
+              ),
+              const SizedBox(height: 24),
+
               DateSelector(
-              selectedDate: _selectedDate,
-              onDateSelected: _onDateSelected,
-          ),
-              const SizedBox(height: 20),
+                selectedDate: _selectedDate,
+                onDateSelected: _onDateSelected,
+              ),
+              const SizedBox(height: 30),
+
               Center(
-                child: ElevatedButton(
+                child: CustomButton(
+                  text: "Register",
+                  textColor: Colors.white,
+                  backgroundColor: Colors.teal,
                   onPressed: _onRegister,
-                  child: const Text('Register'),
                 ),
               ),
             ],
